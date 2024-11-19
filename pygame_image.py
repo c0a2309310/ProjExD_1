7423bad3ae93d1ad469524e472d3c59d03ba7b9c
Takano <c0a2309310@edu.teu.ac.jp>
2024-11-19 15:32:09 +0900

    練習2:こうかとん読み込み

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img2,True,False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
@@ -17,11 +19,15 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        
+        screen.blit(bg_img, [0+tmr, 0])
+        bg_width = int(bg_img.get_width()/(tmr+1))
+        screen.blit(bg_img2,[bg_width,0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        # if tmr == 800:
+        #     tmr = 0      
         clock.tick(200)
 
 
